8954c1e3b1b0e3b650341c3bdcfc9d7163bf9063
ismaelFlores <isma.flores.4.66@gmail.com>
2022-05-27 21:36:36 -0300

    first commit

---
+++ src/main/java/ar/edu/unju/fi/ejercicios/ProyectoIntegradorPvApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.ejercicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntegradorPvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIntegradorPvApplication.class, args);
+	}
+
+}
