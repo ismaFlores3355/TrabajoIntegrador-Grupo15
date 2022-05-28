a3d07c78380f8588d2557df545b17963b67b720f
ismaelFlores <isma.flores.4.66@gmail.com>
2022-05-28 19:54:47 -0300

    agregando paginas

--- src/main/java/ar/edu/unju/fi/controller/InicioController.java
+++ src/main/java/ar/edu/unju/fi/controller/InicioController.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class InicioController {
 
-	@GetMapping("/inicio")
-	public String getIndexPage(Model model) {
-		return "index";
-	}
+
 	
 }
