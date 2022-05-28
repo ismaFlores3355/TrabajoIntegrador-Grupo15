83d39e16522dc86142003f2058eee2f162d0fb84
ismaelFlores <isma.flores.4.66@gmail.com>
2022-05-28 19:53:47 -0300

    agregando html - Bootstrap

--- src/main/java/ar/edu/unju/fi/ejercicios/ProyectoIntegradorPvApplication.java
+++ src/main/java/ar/edu/unju/fi/ejercicios/ProyectoIntegradorPvApplication.java
@@ -2,8 +2,10 @@ package ar.edu.unju.fi.ejercicios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"ar.edu.unju.fi.controller"})
 public class ProyectoIntegradorPvApplication {
 
 	public static void main(String[] args) {
